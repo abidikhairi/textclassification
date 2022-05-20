d1abcffd2d2bdf3969b9b01f1c28b382a4f1f1a8
abidikhairi <abidikhairi.1337@gmail.com>
2022-05-20 13:56:54 +0100

    add: LSTM, GRU Models

--- models/reccurent.py
+++ models/reccurent.py
@@ -1,26 +1,28 @@
 import torch as th
 from torch import nn
 
-class RNNModel(nn.Module):
+class BaseModel(nn.Module):
     def __init__(self, vocab_size, embedding_dim, padding_idx, hidden_dim, num_classes) -> None:
         super().__init__()
 
         self.hidden_dim = hidden_dim
-
+        self.reccurent_layer = None
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim, padding_idx=padding_idx)
-        self.rnn = nn.RNN(embedding_dim, hidden_dim)
         self.fc = nn.Linear(hidden_dim, num_classes, bias=False)
         self.softmax = nn.Softmax(dim=1)
 
     def forward(self, seq):
+        
+        if self.reccurent_layer is None:
+            raise NotImplementedError('Recurrent layer is not initialized')
+
         word_embeds = self.word_embeddings(seq) # shape (L, Hin)
         h_0 = self.init_hidden()
 
-        # rnn_out shape (L, Hout), hidden shape (1, Hout)
-        rnn_out, hidden = self.rnn(word_embeds, h_0)
-
+        # out shape (L, Hout), hidden shape (1, Hout)
+        out, _ = self.reccurent_layer(word_embeds, h_0)
         # use \hat{y}_T to make predictions
-        inp = rnn_out[-1].view(1, -1)
+        inp = out[-1].view(1, -1)
         out = self.fc(inp)
 
         return self.softmax(out)
@@ -28,3 +30,39 @@ class RNNModel(nn.Module):
 
     def init_hidden(self):
         return th.zeros(1, self.hidden_dim, device=th.device('cuda' if th.cuda.is_available() else 'cpu'))
+
+class RNNModel(BaseModel):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        
+        embedding_dim = kwargs['embedding_dim']
+        hidden_dim = kwargs['hidden_dim']
+
+        self.reccurent_layer = nn.RNN(embedding_dim, hidden_dim)
+
+    def forward(self, seq):
+        return super().forward(seq)
+
+class LSTMModel(BaseModel):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        embedding_dim = kwargs['embedding_dim']
+        hidden_dim = kwargs['hidden_dim']
+
+        self.rnn = nn.LSTM(embedding_dim, hidden_dim)
+
+    def forward(self, seq):
+        return super().forward(seq)
+
+class GRUModel(BaseModel):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+
+        embedding_dim = kwargs['embedding_dim']
+        hidden_dim = kwargs['hidden_dim']
+
+        self.rnn = nn.GRU(embedding_dim, hidden_dim)
+
+    def forward(self, seq):
+        return super().forward(seq)
